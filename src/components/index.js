7ede753929639c795c33e482b071cd2bbdf2da8a
ciotas <linzikristy@qq.com>
2020-05-05 16:53:41 +0800

    topics

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,14 @@
 import Vue from 'vue'
 import Message from './Message'
+import Modal from './Modal'
+import Pagination from './Pagination'
 
-Vue.component('Message', Message)
+const components = {
+  Message,
+  Modal,
+  Pagination
+}
+
+for (const [key, value] of Object.entries(components)) {
+  Vue.component(key, value)
+}
